c348e70730748212b3defa564bd279de4f7c64bd
shaolang <shaolang@ymail.com>
2024-03-31 17:20:17 +0800

    Complete step 2: data cleaning

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ _steps = [
 
 
 # This automatically reads in the configuration
-@hydra.main(config_name='config')
+@hydra.main(config_name='config', version_base='1.1')
 def go(config: DictConfig):
 
     # Setup the wandb experiment. All runs will be grouped under this name
@@ -53,7 +53,18 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                 os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                 "main",
+                 parameters={
+                     "input_artifact": "sample.csv:latest",
+                     "output_artifact": "clean_sample.csv",
+                     "output_type": "clean_sample",
+                     "output_description": "Data with outliers and null values removed",
+                     "min_price": config['etl']['min_price'],
+                     "max_price": config['etl']['max_price']
+                 },
+            )
 
         if "data_check" in active_steps:
             ##################
--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -2,7 +2,10 @@
 """
 Performs basic cleaning on the data and save the results in Weights & Biases
 """
+from typing import cast
+from wandb.wandb_run import Run
 import argparse
+import pandas as pd
 import logging
 import wandb
 
@@ -13,7 +16,7 @@ logger = logging.getLogger()
 
 def go(args):
 
-    run = wandb.init(job_type="basic_cleaning")
+    run = cast(Run, wandb.init(job_type="basic_cleaning"))
     run.config.update(args)
 
     # Download input artifact. This will also log that this script is using this
@@ -23,6 +26,21 @@ def go(args):
     ######################
     # YOUR CODE HERE     #
     ######################
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
+    df = df[df['price'].between(args.min_price, args.max_price)].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    df.to_csv('clean_sample.csv', index=False)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description
+    )
+    artifact.add_file('clean_sample.csv')
+    run.log_artifact(artifact)
+
+    run.finish()
 
 
 if __name__ == "__main__":
@@ -31,26 +49,46 @@ if __name__ == "__main__":
 
 
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact",
+        type=str, ## INSERT TYPE HERE: str, float or int,
+        help='Input artifact', ## INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_artifact",
+        type=str, ## INSERT TYPE HERE: str, float or int,
+        help='Output artifact', ## INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_type",
+        type=str, ## INSERT TYPE HERE: str, float or int,
+        help='Type of output artifact', ## INSERT DESCRIPTION HERE,
         required=True
     )
 
+    parser.add_argument(
+        "--output_description",
+        type=str, ## INSERT TYPE HERE: str, float or int,
+        help='Description of output artifact', ## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=float, ## INSERT TYPE HERE: str, float or int,
+        help='Minimum price', ## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=float, ## INSERT TYPE HERE: str, float or int,
+        help='Maximum price', ## INSERT DESCRIPTION HERE,
+        required=True
+    )
 
     args = parser.parse_args()
 
